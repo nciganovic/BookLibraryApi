02d6391759efdd191b2ee7f32eb8cf83b534ee53
nciganovic <nciganovic99@gmail.com>
2022-05-07 15:43:06 +0200

    Add Entity not found exception

---
+++ BookLibrary/Applicaiton/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Application.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        private int id;
+        private string tableName;
+
+        public EntityNotFoundException(int id, string tableName)
+        {
+            this.id = id;
+            this.tableName = tableName;
+        }
+
+        public override string Message => "Entity with id = " + id + " does not exist in table " + tableName;
+    }
+}
