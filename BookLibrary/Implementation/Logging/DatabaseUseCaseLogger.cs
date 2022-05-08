d31c351305d948dda297e0629f43664a21157505
nciganovic <nciganovic99@gmail.com>
2022-05-08 14:18:29 +0200

    database use case logger

---
+++ BookLibrary/Implementation/Logging/DatabaseUseCaseLogger.cs
@@ -0,0 +1,31 @@
+﻿using Application.Interfaces;
+using DataAccess;
+using System;
+using Domain;
+using Newtonsoft.Json;
+
+namespace Implementation.Logging
+{
+    public class DatabaseUseCaseLogger : IUseCaseLogger
+    {
+        private BookLibraryContext _context; 
+
+        public DatabaseUseCaseLogger(BookLibraryContext context)
+        {
+            _context = context;
+        }
+
+        public void Log(IUseCase useCase, IApplicationActor applicationActor, object useCaseData)
+        {
+            _context.UseCaseLogs.Add(new UseCaseLog
+            {
+                Actor = applicationActor.Identity,
+                Data = JsonConvert.SerializeObject(useCase),
+                Date = DateTime.Now,
+                UseCaseName = useCase.Name
+            });
+
+            _context.SaveChanges();
+        }
+    }
+}
