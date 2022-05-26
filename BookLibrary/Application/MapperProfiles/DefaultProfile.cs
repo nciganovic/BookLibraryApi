8cd4c07d1786a70350a86691e07112b7a717976e
nciganovic <nciganovic99@gmail.com>
2022-05-27 00:03:20 +0200

    Add Mappings

--- BookLibrary/Application/MapperProfiles/DefaultProfile.cs
+++ BookLibrary/Application/MapperProfiles/DefaultProfile.cs
@@ -1,11 +1,12 @@
-﻿using Application.Dto;
+﻿using Application.Dto.Author;
+using Application.Dto.Category;
+using Application.Dto.Format;
+using Application.Dto.Language;
+using Application.Dto.Membership;
+using Application.Dto.Publisher;
+using Application.Dto.Role;
 using AutoMapper;
 using Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.MapperProfiles
 {
@@ -16,6 +17,30 @@ namespace Application.MapperProfiles
             CreateMap<AddMembershipDto, Membership>();
             CreateMap<ChangeMembershipDto, Membership>();
             CreateMap<Membership, MembershipResultDto>();
+
+            CreateMap<AddRoleDto, Role>();
+            CreateMap<ChangeRoleDto, Membership>();
+            CreateMap<Role, RoleResultDto>();
+
+            CreateMap<AddLanguageDto, Language>();
+            CreateMap<ChangeLanguageDto, Membership>();
+            CreateMap<Language, LanguageResultDto>();
+
+            CreateMap<AddPublisherDto, Publisher>();
+            CreateMap<ChangePublisherDto, Publisher>();
+            CreateMap<Publisher, PublisherResultDto>();
+
+            CreateMap<AddFormatDto, Format>();
+            CreateMap<ChangeFormatDto, Format>();
+            CreateMap<Format, FormatResultDto>();
+
+            CreateMap<AddCategoryDto, Category>();
+            CreateMap<ChangeCategoryDto, Category>();
+            CreateMap<Category, CategoryResultDto>();
+
+            CreateMap<AddAuthorDto, Author>();
+            CreateMap<ChangeAuthorDto, Author>();
+            CreateMap<Author, AuthorResultDto>();
         }
     }
 }
