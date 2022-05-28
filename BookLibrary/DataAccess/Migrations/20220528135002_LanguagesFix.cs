15a721b3e184d281e53eb3b2781db89434a621f0
nciganovic <nciganovic99@gmail.com>
2022-05-28 18:16:07 +0200

    Add 's' to Languages

---
+++ BookLibrary/DataAccess/Migrations/20220528135002_LanguagesFix.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class LanguagesFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Language_LanguageId",
+                table: "Books");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Language",
+                table: "Language");
+
+            migrationBuilder.RenameTable(
+                name: "Language",
+                newName: "Languages");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Language_Name",
+                table: "Languages",
+                newName: "IX_Languages_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Languages",
+                table: "Languages",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Languages_LanguageId",
+                table: "Books",
+                column: "LanguageId",
+                principalTable: "Languages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Languages_LanguageId",
+                table: "Books");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Languages",
+                table: "Languages");
+
+            migrationBuilder.RenameTable(
+                name: "Languages",
+                newName: "Language");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Languages_Name",
+                table: "Language",
+                newName: "IX_Language_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Language",
+                table: "Language",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Language_LanguageId",
+                table: "Books",
+                column: "LanguageId",
+                principalTable: "Language",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
