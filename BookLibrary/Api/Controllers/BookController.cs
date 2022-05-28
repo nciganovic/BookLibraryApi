2ea791e6fe0cae07c49d255e0cfecdbb8a446c70
nciganovic <nciganovic99@gmail.com>
2022-05-28 19:19:37 +0200

    Add book controller

---
+++ BookLibrary/Api/Controllers/BookController.cs
@@ -0,0 +1,60 @@
+﻿using Application.Dto.Book;
+using AutoMapper;
+using DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BookLibraryContext _context;
+        private IMapper _mapper;
+
+        public BookController(BookLibraryContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        // GET: api/<BookController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<BookController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<BookController>
+        [HttpPost]
+        public void Post([FromBody] AddBookDto dto)
+        {
+
+        }
+
+        // PUT api/<BookController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BookController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
