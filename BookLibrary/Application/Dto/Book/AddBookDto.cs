1701bb4af932fcd87773fee2dac3b5ad550db285
nciganovic <nciganovic99@gmail.com>
2022-05-28 19:19:48 +0200

    Book Dtos

---
+++ BookLibrary/Application/Dto/Book/AddBookDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.Dto.Book
+{
+    public class AddBookDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Pages { get; set; }
+        public int AvailableUnits { get; set; }
+        public string CoverImageSource { get; set; }
+        public string ContentFileSource { get; set; }
+        public int Year { get; set; }
+        public int PublisherId { get; set; }
+        public int LanguageId { get; set; }
+        public int CategoryId { get; set; }
+        public int FormatId { get; set; }
+        public int[] AuthorIds { get; set; }
+    }
+}
---
+++ BookLibrary/Application/Dto/Book/BookResultDto.cs
@@ -0,0 +1,29 @@
+﻿using Application.Dto.Category;
+using Application.Dto.Format;
+using Application.Dto.Language;
+using Application.Dto.Publisher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dto.Book
+{
+    public class BookResultDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Pages { get; set; }
+        public int AvailableUnits { get; set; }
+        public string CoverImageSource { get; set; }
+        public string ContentFileSource { get; set; }
+        public int Year { get; set; }
+        public PublisherResultDto Publisher { get; set; }
+        public LanguageResultDto Language { get; set; }
+        public CategoryResultDto Category { get; set; }
+        public FormatResultDto Format { get; set; }
+        public List<string> Authors { get; set; }
+    }
+}
