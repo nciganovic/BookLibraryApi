588627393c0fdf9cbf2c705e37edbb9424864924
nciganovic <nciganovic99@gmail.com>
2022-05-31 22:08:38 +0200

    set image and file optional

--- BookLibrary/DataAccess/Migrations/20220529110854_SetImageAndFileOptional.Designer.cs
+++ BookLibrary/DataAccess/Migrations/20220529110854_SetImageAndFileOptional.Designer.cs
@@ -4,14 +4,16 @@ using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(BookLibraryContext))]
-    partial class BookLibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20220529110854_SetImageAndFileOptional")]
+    partial class SetImageAndFileOptional
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
